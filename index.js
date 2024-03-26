f5b91eeb700d40e04b0ea9841cf603c3ef0d0f9c
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-03-26 11:17:50 +0100

    add eventlistener for form submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('#form')
+const nameInput = document.querySelector('#name')
+const list = document.querySelector('#list')
+const emailInput = document.querySelector('#email')
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const li = document.createElement('li')
+    li.innerText = `Name: ${nameInput.value}\n  Email: ${emailInput.value}`
+
+    list.append(li)
+
+    nameInput.value = ''
+    emailInput.value = ''
+})
